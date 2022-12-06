a23e1e9f5fbeda35b8cd373f42b65f722d735aec
Bob Ruiken <bobruiken@gmail.com>
2022-12-06 12:19:33 +0100

    Even a little shorter day 6

--- day6/day6.1.py
+++ day6/day6.1.py
@@ -1,1 +1,1 @@
-print([(l:=input()),[i+4 for i in range(len(l))if len({*l[i:i+4]})>3]][1][0])
+print((l:=input(),*[i+4 for i in range(len(l))if len({*l[i:i+4]})>3])[1])
--- day6/day6.2.py
+++ day6/day6.2.py
@@ -1,1 +1,1 @@
-print([(l:=input()),[i+14 for i in range(len(l))if len({*l[i:i+14]})>13]][1][0])
+print((l:=input(),*[i+14 for i in range(len(l))if len({*l[i:i+14]})>13])[1])
