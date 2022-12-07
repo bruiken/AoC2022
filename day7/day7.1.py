75cc3795414cb3d8147c3a56391b6f35733fde54
Bob Ruiken <bobruiken@gmail.com>
2022-12-07 12:10:41 +0100

    Tiny bit simpler day 7

--- day7/day7.1.py
+++ day7/day7.1.py
@@ -1,24 +1,20 @@
 cur_sizes = [0]
-depth = 0
 res = 0
 for line in open(0).readlines()[1:]:
     fst, *args = line.split()
     if fst == '$':
         if args[0] == 'cd':
             if args[1] == '..':
-                depth -= 1
                 if cur_sizes[-1] <= 1e5:
                     res += cur_sizes[-1]
-                cur_sizes[depth] += cur_sizes.pop()
+                cur_sizes[len(cur_sizes)-2] += cur_sizes.pop()
             else:
-                depth += 1
                 cur_sizes += [0]
     elif fst != 'dir':
-        cur_sizes[depth] += int(fst)
+        cur_sizes[-1] += int(fst)
 for i in range(len(cur_sizes)-1,-1,-1):
     if cur_sizes[i] <= 1e5:
         res += cur_sizes[i]
     if i > 0:
         cur_sizes[i-1] += cur_sizes.pop()
-
 print(res)
--- day7/day7.2.py
+++ day7/day7.2.py
@@ -1,26 +1,23 @@
 cur_sizes = [0]
-depth = 0
 dir_sizes = []
 for line in open(0).readlines()[1:]:
     fst, *args = line.split()
     if fst == '$':
         if args[0] == 'cd':
             if args[1] == '..':
-                depth -= 1
                 dir_sizes.append(cur_sizes[-1])
-                cur_sizes[depth] += cur_sizes.pop()
+                cur_sizes[len(cur_sizes)-2] += cur_sizes.pop()
             else:
-                depth += 1
                 cur_sizes += [0]
     elif fst != 'dir':
-        cur_sizes[depth] += int(fst)
+        cur_sizes[-1] += int(fst)
 for i in range(len(cur_sizes)-1,-1,-1):
     dir_sizes.append(cur_sizes[-1])
     if i > 0:
         cur_sizes[i-1] += cur_sizes.pop()
 
 space_needed = 3e7 - (7e7 - dir_sizes[-1])
-cur_best, rem_dir = 7e7, ''
+cur_best, rem_dir = 7e7, -1
 for d, s in enumerate(dir_sizes):
     if s >= space_needed:
         if s - space_needed < cur_best:
